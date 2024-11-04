e72435ec8691751e68bf611f08778a51b1015f40
Carine Morel <carine@tarides.com>
2024-11-04 22:08:31 +0100

    Remove picos deps.

--- lib/actor_picos_io_cohttp/actor_picos_io_cohttp.mli
+++
@@ -1,7 +0,0 @@
-type client
-
-val client : ?uri:string -> username:string -> unit -> client
-(** [client ~uri:"localhost:8080" ~username:"your-pseudo" ()] returns a [client] connected to server reachable at [uri]. *)
-
-include Actor_shared.S with type client := client
-(** @inline *)
